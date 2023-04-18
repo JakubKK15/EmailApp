2753e989fb2744ad597a2f9146e78a10e091ca2d
Jakub Kwadrans <127778202+JakubKK15@users.noreply.github.com>
2023-04-18 03:16:15 +0200

    Finished the app.
    Cleaned up the code, and made sure everything is working. This is most likely not the final version of the app, as I will most likely add database integration.

--- src/Email.java
+++ src/Email.java
@@ -3,38 +3,36 @@ import java.util.Scanner;
 public class Email {
     private final String firstName;
     private final String lastName;
-    private final String password;
     private final int defaultPasswordLength = 10;
     private final String companySuffix = "abcompany.com";
     private final String email;
+    private String password;
+    private int mailboxCapacity = 500;
     private String department;
     private String altEmail;
-    private int mailboxCapacity;
 
     // Constructor to receive the first and last name
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("Email created.");
 
         // Call a method asking for the department - return the department
         this.department = setDepartment();
-        System.out.println("The department is: " + department);
 
         this.password = setRandomPassword(defaultPasswordLength);
-        System.out.println("Your password is: " + password);
+        System.out.println("Password: " + password);
 
         this.email = this.firstName.toLowerCase() + "." +
                 this.lastName.toLowerCase() + "@" +
                 this.department.toLowerCase() + "." +
                 this.companySuffix;
-        System.out.println("Your email is: " + email);
     }
 
     // Ask for the department
 
     private String setDepartment() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("New Worker: " + firstName);
         System.out.print("""
                 DEPARTMENT CODES
                 1) Sales
@@ -50,7 +48,7 @@ public class Email {
         } else if (depChoice == 3) {
             department = "Accounting";
         } else if (depChoice == 0) {
-            department = null;
+            department = "";
         } else {
             System.out.println("Enter a correct number.");
             setDepartment();
@@ -69,10 +67,39 @@ public class Email {
         }
         return new String(password);
     }
-    // Set the mailbox capacity
+
+    // Change the password
+    public void changePassword(String password) {
+        this.password = password;
+    }
 
     // Set the alternate email
 
-    // Change the password
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
 
+    // Set the mailbox capacity
+    public void setMailboxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+    }
+
+    public String getAltEmail() {
+        return altEmail;
+    }
+
+    public void setAltEmail(String altEmail) {
+        this.altEmail = altEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "Info{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", mailboxCapacity=" + mailboxCapacity + "mb" +
+                ", altEmail='" + altEmail + '\'' +
+                '}';
+    }
 }
--- src/EmailApp.java
+++ src/EmailApp.java
@@ -1,6 +1,10 @@
 public class EmailApp {
-  public static void main(String[] args) {
-    Email em1 = new Email("John", "Smith");
-  }
+    public static void main(String[] args) {
+        Email em1 = new Email("John", "Smith");
+
+        em1.setAltEmail("js@gmail.com");
+        System.out.println(em1.getAltEmail());
+        System.out.println(em1);
+    }
 }
 
